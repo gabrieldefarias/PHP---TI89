33c51dfe40ea158254bdd9ee812b1dbb1074baca
Gabrieldefarias <gabrielfoliveira2020@gmail.com>
2020-08-04 11:32:01 -0300

    04/08 - Ajuste de posição

--- sessões/sistema_cadastro/op_login.php
+++ sessões/sistema_cadastro/op_login.php
@@ -3,22 +3,20 @@
     session_start();
 
     if(isset($_POST['logar'])){
+        $txt_usuario=$_POST['txt_usuario'];
+        $text_senha = $_POST['txt_senha'];
 
-    $txt_usuario=$_POST['txt_usuario'];
-    $text_senha = $_POST['txt_senha'];
+        $text_senha =md5($text_senha );
+        $sql ="SELECT * FROM usuario where login ='$txt_usuario' and senha='$text_senha' ";
 
-    $text_senha =md5($text_senha );
+        $resultado = mysqli_query($conn,$sql);
 
-    $sql ="SELECT * FROM usuario where login ='$txt_usuario' and senha='$text_senha' ";
-
-    $resultado = mysqli_query($conn,$sql);
-
-    if (mysqli_num_rows($resultado)>0){
-        $_SESSION['logou'] = 1;
-        header('location:restrito.php');
-    }else{  
-        $_SESSION['logou'] = 0;
-        header('location:frmlogin.php');
+        if (mysqli_num_rows($resultado)>0){
+            $_SESSION['logou'] = 1;
+            header('location:restrito.php');
+        }else{  
+            $_SESSION['logou'] = 0;
+            header('location:frmlogin.php');
+        }
     }
-}
 ?>
